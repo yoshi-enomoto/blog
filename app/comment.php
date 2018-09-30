08207c6a276d340c54871eb7e68a0f1845d4422e
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-09-30 17:24:51 +0900

    モデルの設定
    ・リレショート
    ・書き込みカラムの指定許可

--- app/comment.php
+++ app/comment.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use \App\Post;
 
+// モデル生成時、名前は大文字にする！
 class comment extends Model
 {
-    //
+    protected $fillable = ["body"];
+
+    // リレーションの設定
+      // $comment->post
+      // →$commentからpostへアクセスする。belongsかhasかで、メソッド名は単数・複数となる！
+    public function post() {
+      return $this->belongsTo('Post');
+    }
 }
--- app/post.php
+++ app/post.php
@@ -3,7 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use \App\Comment;
 
+// モデル生成時、名前は大文字にする！
 class post extends Model
 {
     // MassAssigmentException 設定をする。
@@ -11,4 +13,11 @@ class post extends Model
       // ＝（悪意のあるユーザーが、本来設定できないカラムの値を設定する攻撃。）
     protected $fillable = ["title", "body"];
       // railsのstrong parameter
+
+    // リレーションの設定
+    // $post->comments
+      // →$postからcommentsへアクセスする。belongsかhasかで、メソッド名は単数・複数となる！
+    public function comments() {
+      return $this->hasMany('Comment');
+    }
 }
