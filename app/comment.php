3b73bcaa2df8ba3b96f54f7a50183adc42c48626
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-10-20 16:34:14 +0900

    commentsテーブルにも『deleted_at』カラムを追加

--- app/comment.php
+++ app/comment.php
@@ -3,11 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 // モデル生成時、名前は大文字にする！
 class comment extends Model
 {
+    use softDeletes;
+
     protected $fillable = ["body"];
+
+    protected $dates =
+    [
+        // カラムの設定
+        'deleted_at',
+    ];
 
     // リレーションの設定
       // $comment->post
