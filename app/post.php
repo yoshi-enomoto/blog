c36c7c89cf592251d54b4cefd327384b95cc388a
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-10-20 16:29:42 +0900

    softDeletesを追加
    1:追加用のマイグレファイル生成
    2:そのマイグレファイルに『$table->softDeletes();』を追記
    3:モデルにカラムの設定を追記
    　・softDeletesの読み込み
    　・softDeletesの使用記述
    　・$datesにカラムの記述

--- app/post.php
+++ app/post.php
@@ -5,15 +5,28 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 // use \App\comment;
     // 『use』を用いてリレーションを簡略化して記述しようと思ったが、失敗した。原因追求できず。
+use Illuminate\Database\Eloquent\SoftDeletes;
+// 『softDelets』の読み込み記述
+
 
 // モデル生成時、名前は大文字にする！
 class post extends Model
 {
+    use SoftDeletes;
+    // 『softDelets』の使用記述
+
     // MassAssigmentException 設定をする。
       // Laravelではデフォルトで、意図しないリクエストによって悪意のあるデータが挿入されてしまう脆弱性（MassAssigment）の対策が有効になっている。
       // ＝（悪意のあるユーザーが、本来設定できないカラムの値を設定する攻撃。）
     protected $fillable = ["title", "body"];
       // railsのstrong parameter
+
+    protected $dates =
+    [
+        // カラムの設定
+        'deleted_at',
+    ];
+
 
     // リレーションの設定
     // $post->comments
