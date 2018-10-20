c97210a26e58a5f49cb519ebc13f324b95e33355
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-10-20 16:12:02 +0900

    バリデーションメッセを詳細に作る場合

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -37,7 +37,11 @@ class StorePostRequest extends FormRequest
         // validationのエラーメッセージをカスタマイズしたい場合に記述。
     {
         return [
-            'title.required' => 'please enter title!!!'
+            // Requestに記述することで、validation.phpをオーバーライドすることが可能。
+            'title.required' => 'please enter title!!!',
+            // 複数設定している場合、上記と下記のようにセクションを分けて記述する
+            // 下記をコメントアウトすれば、validation.phpの方が適用される。
+            'title.min' => 'タイトルは最小でも3文字ですよ〜',
         ];
     }
 
