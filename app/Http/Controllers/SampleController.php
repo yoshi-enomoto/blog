0485540711b37900de5a2eb241a755ce66eba4eb
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-10-25 00:28:19 +0900

    実際に送信する為のコントローラー設定

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -4,7 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+// Mailクラスのuse（Mail::toで使用）
+use Illuminate\Support\Facades\Mail;
+// SampleNotificationクラスのuse（インスタンス？としてnewする為）
+use App\Mail\SampleNotification;
+
 class SampleController extends Controller
 {
-    //
+    // 送信URLが叩かれた時に動くアクションの設定
+    public function SampleNotification()
+    {
+        $name = 'ララベル太郎';
+        $text = 'これからもよろしくお願いいたします。';
+        $to = 'test@gmail.com';
+
+        // Mailクラスのto()メソッド
+        // かつ、sendメソッドでSampleNotificationクラスのインスタンスを渡す
+        Mail::to($to)->send(new SampleNotification($name, $text));
+    }
 }
