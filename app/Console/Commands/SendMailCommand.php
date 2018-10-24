651eeee23ecfe9ede6bb445291009ecc4fa6e665
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-10-25 02:09:53 +0900

    コマンド詳細作成

--- app/Console/Commands/SendMailCommand.php
+++ app/Console/Commands/SendMailCommand.php
@@ -11,14 +11,38 @@ class SendMailCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'command:name';
+    // 登録するコマンド名
+    protected $signature = 'app:email:send';
+    // protected $signature = 'command:name';
+        // command：グルーピング、name：この処理の識別子
+
+    // どのように使用するか分かっていない為、コメアウト
+    // /**
+    // * @var App\Mailer
+    // */
+    // protected $mailer;
+
+    // *
+    //  * Create a new command instance.
+    //  *
+    //  * @param App\Mailer $mailer
+    //  * @return void
+
+    // public function __construct(Mailer $mailer)
+    // {
+    //     parent::__construct();
+
+    //     $this->mailer = $mailer;
+    // }
+
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    // php artisan list時に表示されるこのコマンドの説明
+    protected $description = 'send an email to subscriber';
 
     /**
      * Create a new command instance.
@@ -35,8 +59,12 @@ class SendMailCommand extends Command
      *
      * @return mixed
      */
+    // このコマンドを実行するたびに呼び出される処理
     public function handle()
     {
-        //
+        $name = 'コマンド次郎';
+        $text = '今後ともご贔屓に。';
+        $to = 'try@gmail.com';
+        Mail::to($to)->send(new SampleNotification($name, $text));
     }
 }
