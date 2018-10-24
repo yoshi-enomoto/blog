c8e213808c65a603b1049e6b7fc12a56e3928fb3
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-10-25 00:07:32 +0900

    SampleNotification.phpへの基本記述

--- app/Mail/SampleNotification.php
+++ app/Mail/SampleNotification.php
@@ -11,14 +11,26 @@ class SampleNotification extends Mailable
 {
     use Queueable, SerializesModels;
 
+    // メンバ変数宣言（格納用）
+    protected $title;
+    protected $text;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    // public function __construct()
+    public function __construct($name='テスト', $text='テストです。')
+        // メール本文プレビュー用に、仮で引数を渡す。
+        // コンストラクタで引数を使用している為
     {
-        //
+        // 処理発動時に、引数をとってメンバ変数へ格納する
+        $this->title = sprintf('%sさん、ありがとうございます。', $name);
+            // sprintf(文字列のフォーマット, 入力文字１, 入力文字２,)のフォーマット
+            // 『%s』なら文字列、『%d』なら数値を代入。その他色々あり。
+            // <ex>sprintf("%s 君は %s を %d 個食べました。", "太郎", "りんご", 7)
+        $this->text = $text;
     }
 
     /**
@@ -26,8 +38,21 @@ class SampleNotification extends Mailable
      *
      * @return $this
      */
+    // メイン処理
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('emails.sample_notification')
+                        // htmlメールのビューをセット
+                    ->text('emails.sample_notification_plain')
+                        // 平文メールのビューをセット
+                        // 『view』と『text』の両方はセット出来ない？
+                    ->subject($this->title)
+                        // メールのタイトルをセット
+                    ->with([
+                        'text' => $this->text,
+                        // ビューに渡す変数をセット
+                      ]);
+
+        // return $this->view('view.name');：初期記述
     }
 }
