dbf6e0916d01d9d586e140f01b9987612374c808
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-11-08 01:51:12 +0900

    php artisan tinker用のメモ書き

--- app/Node.php
+++ app/Node.php
@@ -8,3 +8,9 @@ class Node extends Model
 {
     protected $fillable = ["name", "parent"];
 }
+
+// 『php artisan tinker』で自己結合する場合
+// \DB::table('nodes as f1')->select('f1.id', 'f1.name', 'f2.name as parent_name')->leftJoin('nodes as f2', 'f1.parent', '=', 'f2.id')->get();
+
+    // 1つのテーブルをf1とf2という別名を付け、f1に対してleftJoinする。
+    // その際、同じカラム名は上書きされてしまうので、『f2.name as parent_name』などで変更してやる。
