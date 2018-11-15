70b64457af810bc51c4f318eb93dae4eb0d8af33
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-11-15 23:50:11 +0900

    『npm run watch』実行の為のbladeファイルへの設定
    ・『default.blade.php』にトークン、css、jsの記述
    ・『default.blade.php』にid=“app”の設定（ルートの設定）

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,11 +6,18 @@
 <html lang="ja">
 <head>
     <meta charset="utf-8">
+    <!-- Headタグ内に足す:vue -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="/css/styles.css">
+    <!-- Headタグ内に足す:vue -->
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
 <body>
-    <div class="container">
+    <div id="app" class="container">
         @yield('content')
     </div>
 </body>
+
+<!-- body タグの最後に足す:vue -->
+<script src=" {{ mix('js/app.js') }} "></script>
