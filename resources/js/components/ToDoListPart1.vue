d549bdf1391e42f411bc7280a8ed6d825195bdc2
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-12-20 00:16:28 +0900

    todolist練習：part3
    ・『完了』押下することで、取消線が記載される

--- resources/js/components/ToDoListPart1.vue
+++ resources/js/components/ToDoListPart1.vue
@@ -4,9 +4,15 @@
         <input type="text" v-model="input">
         <input type="button" value="追加" @click="addText()">
         <ul>
-            <li v-for="list in lists" :key="list.id">{{ list.text }}</li>
+            <!-- <li v-for="list in lists" :key="list.id">{{ list.text }}</li> -->
                 <!-- 『:key』を忘れないようにする！ -->
                 <!-- 『ul』で回すとulが数分出来てしまう為、『li』で回す -->
+
+            <li v-for="list in lists" :key="list.id">
+                <span :class="{ complete: list.isComplete}">{{ list.text }}</span>
+                <input type="button" value="完了" @click="list.isComplete=true">
+                <input type="button" value="削除" @click="">
+            </li>
         </ul>
             デバック用：{{ input }}
     </div>
@@ -17,12 +23,13 @@
         data() {
             return {
                 lists: [
-                    { id: 1, text: "aiko"},
-                    { id: 2, text: "yui"},
-                    { id: 3, text: "miwa"},
+                    { id: 1, text: "aiko", isComplete: false},
+                    { id: 2, text: "yui", isComplete: false},
+                    { id: 3, text: "miwa", isComplete: false},
                 ],
                 input: "",
                 id: 4,
+                isComplete: false,
             }
         },
         methods: {
@@ -35,6 +42,7 @@
                 this.lists.push({
                     id: this.id++,
                     text: this.input,
+                    isComplete: false,
                 });
                 // 追加処理の後、テキストボックスを空にする
                 this.input = "";
@@ -48,5 +56,9 @@
         width: 60%;
         margin: auto;
         text-align: left;
+    }
+    /* complete がついた要素には取り消し線を入れる */
+    .complete{
+        text-decoration: line-through;
     }
 </style>
